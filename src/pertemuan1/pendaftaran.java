66a8fa5f9dfc5d958e8f637486d49af8a9e377ff
win 10 <win 10@DESKTOP-4J8C05N>
2023-03-02 12:01:55 +0700

    [NF] Mahasiswa dan pendaftaran

---
+++ src/pertemuan1/pendaftaran.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pertemuan1;
+
+/**
+ *
+ * @author win 10
+ */
+public class pendaftaran {
+    public static void main(String[] args) {
+       Mahasiswa mhs = new Mahasiswa();
+       
+       mhs.nama="maretha";
+       mhs.nobp="21092053";
+       mhs.kelas="B";
+       mhs.prodi="mi";
+       mhs.jurusan="teknologi informasi";
+       
+       System.out.println("nama : " +mhs.nama+ "\n"
+              + "no bp : "+mhs.nobp+"\n"
+                  + "jurusan : "+mhs.jurusan+"\n"
+                      + "prodi : "+mhs.prodi+"\n"
+                           + "kelas : "+mhs.kelas+"\n");
+       
+       System.out.println("\n");
+       
+       Mahasiswa mhs2 = new Mahasiswa ();
+       mhs2.nama="wiwin";
+       mhs2.nobp="210920";
+       mhs2.kelas="B";
+       mhs2.prodi="mi";
+       mhs2.jurusan="teknologi informasi";
+       
+       System.out.println("nama : " +mhs2.nama+ "\n"
+              + "no bp : "+mhs2.nobp+"\n"
+                  + "jurusan : "+mhs2.jurusan+"\n"
+                      + "prodi : "+mhs2.prodi+"\n"
+                           + "kelas : "+mhs2.kelas+"\n");
+    
+  }
+    
+}
+    
+
